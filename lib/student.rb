07d73a35f5ff4b3f11b9c7395f85a534a18f65b7
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-21 03:01:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,16 @@ class Student
   end
 
   def save
-    
+      if self.id
+    self.update
+  else
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
   end
 
 end
