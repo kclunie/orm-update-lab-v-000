9941113db876889a2b683fcbe19089425c65a0f8
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-21 03:20:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
     new_stud = self.new  
     new_stud.id = row[0]
     new_stud.name =  row[1]
-    new_stud.length = row[2]
+    new_stud.grade = row[2]
     new_stud
     
   end
