72e708c565ba9ad82f09030a283452aa1e1ad2d0
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-21 03:12:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,7 +49,12 @@ class Student
     stud
   end
   
-  def self.new_from_db
+  def self.new_from_db(row)
+     new_song = self.new  # self.new is the same as running Song.new
+  new_song.id = row[0]
+  new_song.name =  row[1]
+  new_song.length = row[2]
+  new_song
     
   end
   
